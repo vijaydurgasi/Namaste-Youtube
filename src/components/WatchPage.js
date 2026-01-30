feadbe496f4d2136352c3a54dd3f67c603deb50e
vijaydurgasi <vijayanu292157@gmail.com>
2026-01-30 14:50:07 +0530

    YT video is played based on ID

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,18 +1,41 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { closeMenu } from '../utils/appSlice';
+import { useSearchParams } from 'react-router-dom';
 
 const WatchPage = () => {
+
+  const [searchParams] = useSearchParams();
+  const videoId = searchParams.get("v");
+  console.log("Video id", searchParams.get("v"));
 
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(closeMenu());
-  });
+  }, [dispatch]);
+
+  if (!videoId) {
+    return <h1>Video Not found</h1>;
+  }
 
   return (
-    <div>WatchPage</div>
-  )
-}
+    <div className="w-full flex mr-6 px-4">
+      <div className="w-full max-w-4xl px-6 py-4">
+        <div className="relative w-full pt-[56.25%]">
+          <iframe
+            className="absolute top-0 left-0 w-full h-full rounded-xl"
+            src={`https://www.youtube.com/embed/${videoId}`}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </div>
+      </div>
+    </div>
+  );
+
+};
 
 export default WatchPage;
